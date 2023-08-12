2bf529fa9d7763bbf9598b82b4cb9095db5480ac
tinon1004 <tinon1004@ajou.ac.kr>
2023-08-12 18:16:50 +0900

    Home 합치기

--- src/components/MainPage/Home.jsx
+++ src/components/MainPage/Home.jsx
@@ -4,17 +4,102 @@ import CustomDateRangePickerDay from "../Component/CustomDateRangePickerDay";
 import Calendartwo from "../Component/Calendartwo";
 import TailCalendar from "../Component/TailCalendar";
 import Groups from "../Component/Groups";
+import Box from "../Component/Box";
+import List from "../Component/List";
+
+import styled from "styled-components";
+
+const Container = styled.div`
+display: flex;
+flex-direction: column;
+margin-top: 156px;
+margin-left: 363px;
+`;
+
+const StyledNoticeBox = styled(Box)`
+width: 684px;
+height: 400px;
+flex-shrink: 0;
+border-radius: 8px;
+background: #FFF;
+box-shadow: 0px 0px 10px 0px rgba(38, 40, 43, 0.05);
+margin-bottom: 40px;
+`;
+
+
+const TaskGrid = styled.div`
+display: grid;
+grid-template-columns: repeat(2, 327px);
+grid-gap: 30px;
+`;
+
+const StyledTaskBox = styled(Box)`
+height: 84px;
+flex-shrink: 0;
+border-radius: 8px;
+background: #FFF;
+box-shadow: 0px 0px 10px 0px rgba(38, 40, 43, 0.05);
+display: flex; 
+justify-content: space-between; 
+align-items: center; 
+padding: 0 18px; 
+position: relative; 
+`;
+
+
+
+
+
 
 const Home = () => {
-  return (
-    <div>
-      <h2>HOMEPage</h2>
-      <Groups />
-      <CustomDateRangePickerDay />
-      <Calendartwo />
-      <TailCalendar />
-    </div>
-  );
+
+  
+  const renderTaskBoxes = () => {
+    return Array.from({ length: 4 }, (_, index) => (
+      <StyledTaskBox key={index}>
+        Task Board {index + 1}
+
+      </StyledTaskBox>
+    ));
+  };
+ 
+  const renderPosts = () => {
+    const postContent = [
+      'Notice 1',
+      'Notice 2',
+      'Notice 3',
+      'Notice 4',
+      'Notice 5',
+    ];
+
+    return postContent.map(content => <List key={content} content={content} />);
+  };
+
+
+
+
+  
+    return (
+      <div>
+        <h2>HOME</h2>
+        <Container>
+          <StyledNoticeBox>
+            <h3>Notice</h3>
+            {renderPosts()}
+          </StyledNoticeBox>
+          <TaskGrid>
+            {renderTaskBoxes()}
+            
+          </TaskGrid>
+          <Groups />
+          <CustomDateRangePickerDay />
+          <Calendartwo />
+          <TailCalendar />
+        </Container>
+      </div>
+    );
+  
+
 };
 
 export default Home;
