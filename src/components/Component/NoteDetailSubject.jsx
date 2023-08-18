d161cd333a6b4c29c9325c2a88b7677efaa9314b
tinon1004 <tinon1004@ajou.ac.kr>
2023-08-18 11:11:38 +0900

    Design: NoteDetail

--- src/components/Component/NoteDetailSubject.jsx
+++ src/components/Component/NoteDetailSubject.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import chevron_left from "../../assets/chevron_left.svg";
-
+import plus_lg from "../../assets/plus_lg.svg";
 
 const Header = styled.header`
   display: flex;
@@ -37,6 +37,51 @@ const NoteContainer = styled.div`
 `;
 
 
+const MakeNoteBody = styled.div`
+    width: 1194px;
+    height: 72px;
+    flex-shrink: 0;
+    margin-top: 32px;
+    display: flex;
+`;
+
+
+const AddNote = styled.div` 
+    display: flex;
+    
+    width: 48px;
+    height: 64px;
+    flex-shrink: 0;
+    border-radius: 2px;
+    border: 1.5px dashed #4849FF;
+    margin-left: 32px;
+    margin-top: 36px;
+
+`;
+
+const PlusImg = styled.img`
+    width: 12px;
+    height: 12px;
+    flex-shrink: 0;
+    margin-left: 18px;
+    margin-top: 26px;
+
+`;
+
+
+const Title = styled.p`
+    color: var(--primary-cobalt, #4849FF);
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    padding-left: 26px;
+    padding-top: 59px;
+   
+`;
+
+
+
 
 
 
@@ -53,8 +98,13 @@ function NoteDetailSubject() {
     
    
     <NoteContainer>
-   
-
+    <MakeNoteBody>
+      <AddNote>
+        <PlusImg src={plus_lg} alt="plus_lg" />
+      </AddNote>
+      <Title>신규 노트 만들기</Title>
+    </MakeNoteBody>
+    <h2>학습지</h2>
     </NoteContainer>
     
     </div>
