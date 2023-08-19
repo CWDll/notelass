04be779c9c6bfff72b54d324b42edacc003696ed
tinon1004 <tinon1004@ajou.ac.kr>
2023-08-19 09:29:15 +0900

    Design: add subject in NoteDetail

--- src/components/Component/NoteDetail.jsx
+++ src/components/Component/NoteDetail.jsx
@@ -3,9 +3,7 @@ import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import plus_lg from "../../assets/plus_lg.svg";
 
-import {
-    SubjectContainer,
-  } from "./GroupsStyle";
+
 
 const NoteContainer = styled.div`
     width: 1194px;
@@ -62,6 +60,78 @@ const Title = styled.p`
    
 `;
 
+const CircleText = styled.div`
+    width: 48px;
+    height: 48px;
+    flex-shrink: 0;
+    border-radius: 50%;
+    background: var(--primary-cobalt, #4849FF);
+    margin-left: 32px;
+    margin-top: 12px;
+    display: flex;
+    justify-content: center;
+
+`;
+
+const PurpleText = styled.p`
+    color: #FFF;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    padding-top: 12px;
+`;
+
+const BoldText = styled.p`
+    color: var(--cool-grayscale-title, #26282B);
+    text-align: center;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+
+`;
+
+const GrayText = styled.p`
+    color: var(--cool-grayscale-placeholder, #9EA4AA);
+    font-size: 12px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+    margin-top: 4px;
+
+`;
+
+const SubjectBody = styled.div`
+    display: flex;
+
+    width: 1194px;
+    height: 72px;
+    flex-shrink: 0;
+    margin-top: 16px;  
+
+`;
+
+const SubjectContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 16px;
+    margin-top: 16px;
+`;
+
+const SubjectBodyWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 30px;
+`;
+
+  // 학교, 학년, 반, 과목 명이 들어갈 변수집단 subjectInfo
+  const subjectInfo = "노트고등학교 3학년 1반 문학";
+  // " "를 기준으로 나누고, 3번째인 과목 명을 가져와 subject에 저장
+  const subject = subjectInfo.split(" ")[3];
+  // subject에서 앞 1글자만 가져와 저장하는 letter
+  const letter = subject.substr(0, 1);
+
 function NoteDetail() {
 
 
@@ -78,9 +148,35 @@ function NoteDetail() {
       </AddNote>
       <Title>신규 노트 만들기</Title>
     </MakeNoteBody>
-    <SubjectContainer  onClick={onClick}> 
-        과목
-    </SubjectContainer> 
+    <SubjectBodyWrapper> 
+            <SubjectBody onClick={onClick}>
+                <CircleText>
+                  <PurpleText>{letter}</PurpleText>
+                </CircleText>
+                <SubjectContainer>
+                  <BoldText>{subjectInfo}</BoldText>
+                  <GrayText>김태연 선생님</GrayText>
+                </SubjectContainer>
+            </SubjectBody>
+            <SubjectBody onClick={onClick}>
+                <CircleText>
+                  <PurpleText>영</PurpleText>
+                </CircleText>
+                <SubjectContainer>
+                  <BoldText>노트고등학교 3학년 1반 영어</BoldText>
+                  <GrayText>티파니 선생님</GrayText>
+                </SubjectContainer>
+            </SubjectBody>
+            <SubjectBody onClick={onClick}>
+                <CircleText>
+                  <PurpleText>확</PurpleText>
+                </CircleText>
+                <SubjectContainer>
+                  <BoldText>노트고등학교 3학년 1반 확률과 통계</BoldText>
+                  <GrayText>이순규 선생님</GrayText>
+                </SubjectContainer>
+            </SubjectBody>
+        </SubjectBodyWrapper> 
   </NoteContainer>
 
 
