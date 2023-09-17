e82d0a7c71b08d630440a414c4fe74d38f2e6e88
leechangjoon <2018110021@dongguk.edu>
2023-09-17 15:48:02 +0900

    Feat: SelectSchool page 생성

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,8 @@ import StudentScoreDetail from "./components/Component/StudentScoreDetail";
 import StudentTaskDetail from "./components/Component/StudentTaskDetail";
 import GroupScoreDetail from "./components/Component/GroupScoreDetail";
 // 로그인페이지
-import LoginPage from "./components/LogInPage/Login";
+import Login from "./components/LogInPage/Login";
+import SelectSchool from "./components/LogInPage/SelectSchool";
 
 function App() {
   return (
@@ -50,7 +51,8 @@ function App() {
           <Route path="/Setting" element={<Setting />} />
         </Route>
         {/* 로그인페이지 추가(위치는 아직 셋팅X) */}
-        <Route path="/Login" element={<LoginPage />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/SelectSchool" element={<SelectSchool />} />
       </Routes>
     </Router>
   );
