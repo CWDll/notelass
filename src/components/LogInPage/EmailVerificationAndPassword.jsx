e735e0b136cf735c7d6c17b94af635439f2b1627
leechangjoon <2018110021@dongguk.edu>
2023-09-18 16:54:59 +0900

    Feat: password show 기능 추가

--- src/components/LogInPage/EmailVerificationAndPassword.jsx
+++ src/components/LogInPage/EmailVerificationAndPassword.jsx
@@ -4,6 +4,14 @@ import styled from "styled-components";
 import Button from "@mui/material/Button";
 // 입력창
 import TextField from "@mui/material/TextField";
+// 비밀번호 숨김/공개 입력창
+import IconButton from "@mui/material/IconButton";
+import Input from "@mui/material/Input";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
 
 const ContainerWidth_1920 = styled.div`
   width: 1920px;
@@ -49,10 +57,18 @@ const NextButton = styled(Button)`
 `;
 
 export default function EmailVerificationAndPassword() {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   return (
     <ContainerWidth_1920>
       <Container>
         <Notelass>회원가입</Notelass>
+
         <TitleText>이메일 주소 입력</TitleText>
         <FlexRow>
           <TextField
@@ -70,21 +86,38 @@ export default function EmailVerificationAndPassword() {
           variant="standard"
           placeholder="인증번호 6자리를 입력해 주세요"
         />
+
         <TitleText>비밀번호 입력</TitleText>
+        <FormControl sx={{ m: 1, width: "25ch" }} variant="standard">
+          {/* <InputLabel htmlFor="standard-adornment-password">
+            Password
+          </InputLabel> */}
+          <Input
+            id="standard-adornment-password"
+            type={showPassword ? "text" : "password"}
+            placeholder="영문, 숫자, 특수기호 포함 8자리 이상"
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+
+        <TitleText>비밀번호 확인</TitleText>
         <TextField
           id="standard-basic"
           //   label="Standard"
           variant="standard"
           placeholder="영문, 숫자, 특수기호 포함 8자리 이상"
         />
-        <TitleText>비밀번호 확인</TitleText>
-        <TextField
-          id="standard-basic"
-          //   label="Standard"
-          variant="standard"
-          placeholder="비밀번호를 한 번 더 입력해 주세요"
-          margin="normal"
-        />
+
         <NextButton type="submit" variant="contained" sx={{ mt: 3, mb: 2 }}>
           회원가입
         </NextButton>
