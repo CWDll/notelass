3782ce73eda9ebcc5093a84ecb104afc84129822
leechangjoon <2018110021@dongguk.edu>
2023-10-04 03:07:33 +0900

    Fix: createStore 취소선 제거(취소선 있어도 이상은 없음)

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from "redux";
+import { legacy_createStore as createStore, combineReducers } from "redux";
 import userInputReducer from "./reducers/userInputReducer";
 
 const rootReducer = combineReducers({
