c0956124e5c228a474e0e075e7d6f36e56af620b
leechangjoon <2018110021@dongguk.edu>
2023-11-30 23:18:27 +0900

    feat: 학생수첩 빈 문자열 혹은 공백 저장 불가

--- src/pages/Student/StudentBookContent.jsx
+++ src/pages/Student/StudentBookContent.jsx
@@ -374,6 +374,12 @@ function StudentBookContent({
     const groupId = selectedGroup; // 이전에 선택된 그룹 ID
     const userId = selectedStudent; // 이전에 선택된 학생 ID
 
+    // inputText가 null이거나 빈 문자열이거나 공백만 있는 경우를 확인
+    if (!inputText || inputText.trim() === "") {
+      alert("내용을 입력하세요.");
+      return; // 여기서 함수를 종료하여 저장을 막습니다.
+    }
+
     const requestBody = {
       content: inputText, // 텍스트 입력 내용
       attitudeScore: attitudeCount, // 태도 점수
