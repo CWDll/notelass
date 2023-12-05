ff845720e84bf0f01c1e7209eee25850d0a2456b
tinon1004 <tinon1004@ajou.ac.kr>
2023-12-05 14:32:29 +0900

    Fix: main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import ReactGA from 'react-ga4';
+ReactGA.initialize("G-QSTTQEGWWQ");
+ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
