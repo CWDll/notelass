a0179d99108e06dfc377bb446d47881fd53e1e90
leechangjoon <2018110021@dongguk.edu>
2024-02-04 06:43:22 +0900

    [Refactor] 과제 생성에서 분류에 맞는 데이터 넣어두기

--- src/components/Component/Notice/AssignInfo.jsx
+++ src/components/Component/Notice/AssignInfo.jsx
@@ -1,29 +1,26 @@
 import React from "react";
 
-import * as S from "src/pages/Style/AssignmentStyle"
+import * as S from "src/pages/Style/AssignmentStyle";
 
-export default function  AssignInfo()
-{
-
-    return (
-        <>
-        <S.SettingBox>
-            <S.SmallTitle>마감기한</S.SmallTitle>
-            <S.SubText>김태연 선생님</S.SubText>
-          </S.SettingBox>
-          <S.SettingBox>
-            <S.SmallTitle>허용된 시도</S.SmallTitle>
-            <S.SubText>2회</S.SubText>
-          </S.SettingBox>
-          <S.SettingBox>
-            <S.SmallTitle>전체 배점</S.SmallTitle>
-            <S.SubText>100점</S.SubText>
-          </S.SettingBox>
-          <S.SettingBox>
-            <S.SmallTitle>할당된 그룹</S.SmallTitle>
-            <S.SubText>노트고등학교 3학년 1반 문학</S.SubText>
-          </S.SettingBox>
-        </>
-    )
-};
-
+export default function AssignInfo() {
+  return (
+    <>
+      <S.SettingBox>
+        <S.SmallTitle>마감기한</S.SmallTitle>
+        <S.SubText>2024.02.04</S.SubText>
+      </S.SettingBox>
+      <S.SettingBox>
+        <S.SmallTitle>허용된 시도</S.SmallTitle>
+        <S.SubText>2회</S.SubText>
+      </S.SettingBox>
+      <S.SettingBox>
+        <S.SmallTitle>전체 배점</S.SmallTitle>
+        <S.SubText>100점</S.SubText>
+      </S.SettingBox>
+      <S.SettingBox>
+        <S.SmallTitle>할당된 그룹</S.SmallTitle>
+        <S.SubText>노트고등학교 3학년 1반 문학</S.SubText>
+      </S.SettingBox>
+    </>
+  );
+}
