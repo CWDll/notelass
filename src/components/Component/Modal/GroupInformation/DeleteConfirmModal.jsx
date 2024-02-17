9dc9d2ea27e41b6e5b543074ecb8be681e15d6e9
leechangjoon <2018110021@dongguk.edu>
2024-02-18 02:47:18 +0900

    [Feat] 삭제 확인 텍스트 비교 로직을 구현한다.

--- src/components/Component/Modal/GroupInformation/DeleteConfirmModal.jsx
+++ src/components/Component/Modal/GroupInformation/DeleteConfirmModal.jsx
@@ -11,14 +11,32 @@ function DeleteConfirmModal({
   };
 
   const [checkText, setCheckText] = useState("");
+  const DeleteButtonClick = (checkText) => {
+    if (checkText === "그룹을 삭제합니다.") {
+      alert("삭제 api 발생");
+    } else {
+      alert("문장이 일치하지 않습니다.");
+      return;
+    }
+  };
 
   return (
     <S.ModalContainer>
       <S.Exit ale="exit" onClick={handleButtonClick} />
       <S.TextContainer>
         <S.Text>정말 삭제할 것인지?</S.Text>
-        <S.ColorText>정말 삭제하시겠습니까?</S.ColorText>
+        <input
+          type="text"
+          placeholder="위의 문장을 적어주세요"
+          value={checkText}
+          onChange={(event) => setCheckText(event.target.value)}
+        />
       </S.TextContainer>
+      <S.ButtonContainer>
+        <S.GrayButton onClick={() => DeleteButtonClick(checkText)}>
+          삭제하기
+        </S.GrayButton>
+      </S.ButtonContainer>
     </S.ModalContainer>
   );
 }
