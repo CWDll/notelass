ced167f8d2e3c1653bd21322ec44a50dfabce211
leechangjoon <2018110021@dongguk.edu>
2024-03-03 06:21:07 +0900

    [Feat] 공지와 학습자료 고유 번호로 렌더링

--- src/components/Component/DetailPage/NoticeDetailContent.jsx
+++ src/components/Component/DetailPage/NoticeDetailContent.jsx
@@ -11,7 +11,7 @@ import NoticeInfo from "../Notice/NoticeInfo";
 
 import RoleContext from "../../../RoleContext";
 
-function NoticeDetailContent(noticeId) {
+function NoticeDetailContent(noticeId, lectureMaterialId) {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [files, setFiles] = useState([]);
@@ -20,7 +20,7 @@ function NoticeDetailContent(noticeId) {
   const [groupId, setGroupId] = useState("");
   // const [noticeId, setNoticeId] = useState("");
   const [isEditMode, setIsEditMode] = useState(false);
-  console.log("sd", noticeId);
+  console.log("sd", noticeId, lectureMaterialId);
 
   const { role } = useContext(RoleContext);
 
@@ -29,30 +29,57 @@ function NoticeDetailContent(noticeId) {
   const info = location.state;
   console.log("ND의 info in NDC:", info);
 
-  useEffect(() => {
-    const getStudentList = async () => {
-      try {
-        const res = await instance.get(
-          `/api/notice/detail?noticeId=${noticeId.noticeId}`
-        );
+  const getMaterialtList = async () => {
+    try {
+      const res = await instance.get(
+        `/api/material/detail?materialId=${noticeId.lectureMaterialId}`
+      );
 
-        if (res.status === 200) {
-          console.log("머지", res.data.result);
-          setGroupId(res.data.result.groupId);
-          setTitle(res.data.result.title);
-          setContent(res.data.result.content);
-          setFiles(res.data.result.files);
-          setTeacher(res.data.result.teacher);
-          setCreDate(res.data.result.createdDate);
-        } else {
-          console.log("테스트 실패");
-        }
-      } catch (error) {
-        console.error("테스트 에러", error);
+      if (res.status === 200) {
+        console.log("머지", res.data.result);
+        setGroupId(res.data.result.groupId);
+        setTitle(res.data.result.title);
+        setContent(res.data.result.content);
+        setFiles(res.data.result.files);
+        setTeacher(res.data.result.teacher);
+        setCreDate(res.data.result.createdDate);
+      } else {
+        console.log("테스트 실패");
       }
-    };
-    getStudentList();
-  }, [noticeId]);
+    } catch (error) {
+      console.error("테스트 에러", error);
+    }
+  };
+
+  const getStudentList = async () => {
+    try {
+      const res = await instance.get(
+        `/api/notice/detail?noticeId=${noticeId.noticeId}`
+      );
+
+      if (res.status === 200) {
+        console.log("머지", res.data.result);
+        setGroupId(res.data.result.groupId);
+        setTitle(res.data.result.title);
+        setContent(res.data.result.content);
+        setFiles(res.data.result.files);
+        setTeacher(res.data.result.teacher);
+        setCreDate(res.data.result.createdDate);
+      } else {
+        console.log("테스트 실패");
+      }
+    } catch (error) {
+      console.error("테스트 에러", error);
+    }
+  };
+
+  useEffect(() => {
+    if (noticeId.noticeId != null) {
+      getStudentList();
+    } else if (lectureMaterialId != null) {
+      getMaterialtList();
+    }
+  }, [noticeId, lectureMaterialId]);
 
   const navigate = useNavigate();
   function toReWrite() {
